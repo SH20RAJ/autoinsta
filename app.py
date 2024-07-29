cd7e6d1072a1c52e670c7e754c6667eb2e863b39
sh20raj <shaswatraj3@gmail.com>
2024-07-29 20:55:05 +0530

    working app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from instagrapi import Client
 
-password = ";ya_#GkM,KND6?$";
-email = "uvfczvl@smartnator.com";
-username = "droidcv1";
+# Your Instagram credentials
+username = "droidcv1"
+password = ";ya_#GkM,KND6?$"
 
+# Initialize the client
 cl = Client()
+
+# Login to Instagram
 cl.login(username, password)
 
-user_id = cl.user_id_from_username(username)
-medias = cl.user_medias(user_id, 20)
+# Path to your video file
+video_path = "sample.mp4"
+# Optional path to your cover image
+# cover_image_path = "cover.jpg"
+# Caption for your video
+caption = "Your caption here"
+
+# Upload the video with the optional cover image and caption
+cl.clip_upload(video_path, caption=caption)
+
+print("Video uploaded successfully!")
